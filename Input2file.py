97d78912783480eb7996cfa518d6b26e22403426
vaishud12 <66488413+vaishud12@users.noreply.github.com>
2020-06-12 19:19:54 +0530

    Add files via upload
    Input output file..

---
+++ Input2file.py
@@ -0,0 +1,14 @@
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+
+print("List1 have the following numbers :",list1,)
+print("The positive numbers in list1 are:")
+for numbers in list1 :
+	if numbers>0:
+		print(numbers)
+
+print("\nList2 have the following numbers :",list1,)
+print("The positive numbers in list2 are:")
+for numbers in list2 :
+	if numbers>0:
+		print(numbers)
